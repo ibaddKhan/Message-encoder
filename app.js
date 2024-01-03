70b13c89f5ae974cc5c35007af681544816a2eec
ibaddKhan <ibadurrehman718@gmail.com>
2024-01-03 06:25:50 +0500

    more encrypted

--- app.js
+++ app.js
@@ -8,26 +8,32 @@ async function encodeMessage(e) {
   let encodedMessage = "";
   let shift = parseInt(prompt("Enter the shift value: "));
   let inputMessage = message.value;
-if (shift > 25 || shift<0  ) {
-  alert("Shift value must be between 0 and 25");
-  return;
-}else if(isNaN(shift) ===true){
-  alert("Shift value must be a number");
-return
-}
+  
+  if (shift > 25 || shift < 0) {
+    alert("Shift value must be between 0 and 25");
+    return;
+  } else if (isNaN(shift) === true) {
+    alert("Shift value must be a number");
+    return;
+  }
+
   for (let i = 0; i < inputMessage.length; i++) {
     let char = inputMessage[i];
+    let charCode = char.charCodeAt(0);
+
     if (char.match(/[a-zA-Z]/)) {
       let isUpperCase = char === char.toUpperCase();
       let asciiOffset = isUpperCase ? 65 : 97;
       encodedMessage += String.fromCharCode(
-        ((char.charCodeAt(0) + shift - asciiOffset) % 26) + asciiOffset
+        ((charCode + shift - asciiOffset) % 26) + asciiOffset
       );
+    } else if (char.match(/[0-9]/)) {
+      encodedMessage += String.fromCharCode((charCode - 48 + shift) % 10 + 48);
     } else {
-      encodedMessage += char;
+      encodedMessage += String.fromCharCode(charCode + shift);
     }
   }
-  
+
   console.log("Encoded Message:", encodedMessage);
   div.innerHTML = "Encoded Message: " + encodedMessage;
 }
@@ -40,16 +46,20 @@ function decodeMessage(e) {
 
   for (let i = 0; i < encodedMessage.length; i++) {
     let char = encodedMessage[i];
+    let charCode = char.charCodeAt(0);
+
     if (char.match(/[a-zA-Z]/)) {
       let isUpperCase = char === char.toUpperCase();
       let asciiOffset = isUpperCase ? 65 : 97;
       decodedMessage += String.fromCharCode(
-        ((char.charCodeAt(0) - shift - asciiOffset + 26) % 26) + asciiOffset
+        ((charCode - shift - asciiOffset + 26) % 26) + asciiOffset
       );
+    } else if (char.match(/[0-9]/)) {
+      decodedMessage += String.fromCharCode((charCode - 48 - shift + 10) % 10 + 48);
     } else {
-      decodedMessage += char;
+      decodedMessage += String.fromCharCode(charCode - shift);
     }
   }
-  div.innerHTML = "Decoded Message: " + decodedMessage;
 
+  div.innerHTML = "Decoded Message: " + decodedMessage;
 }
